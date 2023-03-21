fb306d4797044870fd112bceb66607fa7d089670
Fenrir7734 <hitex1999@gmail.com>
2023-03-21 17:06:57 +0100

    Added swagger

---
+++ StrategyPattern/Example02/src/main/java/com/fenrir/example02/config/OpenApiConfiguration.java
@@ -0,0 +1,10 @@
+package com.fenrir.example02.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "File generator - strategy pattern"))
+public class OpenApiConfiguration {
+}
